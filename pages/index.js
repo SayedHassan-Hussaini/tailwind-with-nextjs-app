a2c6f4c23d8eed37f519fdab55e8b0fec16e4beb
sayedhassan-husssaini <sh.hussaini1378@gmail.com>
2022-03-26 11:25:42 +0430

    setup taiwindcss

--- pages/index.js
+++ pages/index.js
@@ -24,12 +24,12 @@ export default function Home() {
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <p >Find in-depth information about Next.js features and API.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <span className='font-bold'>Learn about Next.js in an interactive course with quizzes!</span>
           </a>
 
           <a
